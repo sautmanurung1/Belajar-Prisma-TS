a22ad217f05ca489e2d6605542924da30ead9c8a
sautmanurung1 <smanurung360@gmail.com>
2023-02-07 21:44:55 +0700

    add new code

--- script.ts
+++ script.ts
@@ -1,3 +1,46 @@
 import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
+async function main() {
+    await prisma.user.deleteMany()
+    const user = await prisma.user.createMany({
+        data: [{
+            name: "Saut Manurung",
+            email: "smanurung360@gmail.com",
+            age: 27,
+        }, {
+            name: "Kristian Wiratama",
+            email: "sautmanurungaja28@gmail.com",
+            age:28,
+        }]
+    })
+
+    console.log(user)
+}
+
+main()
+    .catch(e => {
+        console.error(e.message)
+    })
+    .finally(async () => {
+        await prisma.$disconnect()
+    })
+
+
+async function find() {
+    const user = await prisma.user.findMany({
+        where: {
+            name: "Saut Manurung"
+        },
+    })
+
+    console.log(user)
+}
+
+find() 
+    .catch(e => {
+    console.error(e.message)
+    })
+    .finally(async () => {
+    await prisma.$disconnect()
+    })
